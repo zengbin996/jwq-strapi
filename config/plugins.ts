c3db83f34a28b2a4a808cb7aa855b3dbd74a2e45
Nike <hellozengbin@outlook.com>
2026-04-11 17:39:21 +0800

    腾讯云上传

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,30 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          forcePathStyle: false, // ⭐关键
+          useAccelerateEndpoint: false,
+
+          credentials: {
+            accessKeyId: env('TENCENT_SECRET_ID'),
+            secretAccessKey: env('TENCENT_SECRET_KEY'),
+          },
+
+          region: env('TENCENT_REGION'),
+
+          endpoint: `https://${env('TENCENT_BUCKET')}.cos.${env('TENCENT_REGION')}.myqcloud.com`,
+
+          params: {
+            Bucket: env('TENCENT_BUCKET'),
+          },
+        },
+      },
+    },
+  },
+});
 
 export default config;
